ff0e4d432e390d8eefb54ffd40a9387acd6f68d4
Simon Brage <simonbrage@live.dk>
2023-11-21 20:38:48 +0100

    Set up routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import RoomCreation from './components/RoomCreation';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CreateRoom from './pages/CreateRoom';
+import JoinRoom from './pages/JoinRoom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <RoomCreation />
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create-room" element={<CreateRoom />} />
+        <Route path="/join-room" element={<JoinRoom />} />
+        {/* Define other routes with 'element' prop */}
+      </Routes>
+    </Router>
   );
 }
 
