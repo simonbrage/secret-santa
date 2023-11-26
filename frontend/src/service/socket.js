a95cf5e9ae1a7b8517e47d6bfd0e2806b99d8ecd
Simon Brage <simonbrage@live.dk>
2023-11-26 02:45:20 +0100

    Update REACT_APP_SERVER_URL handling

--- frontend/src/service/socket.js
+++ frontend/src/service/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_SERVER_URL : 'http://localhost:3000';
+const URL = process.env.REACT_APP_SERVER_URL || undefined;
 
 export const socket = io(URL, {
     autoConnect: false
